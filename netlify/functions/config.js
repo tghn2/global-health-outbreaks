4bc3f5d767c997ba5d569ce4a9345803019020aa
samdriver2491 <74094719+samdriver2491@users.noreply.github.com>
2026-03-09 15:05:36 +0000

    Update environment variable names in config.js

--- netlify/functions/config.js
+++ netlify/functions/config.js
@@ -3,10 +3,10 @@ exports.handler = async () => {
     statusCode: 200,
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify({
-      supabaseUrl:    process.env.SUPABASE_URL_TOKEN,
-      supabaseKey:    process.env.SUPABASE_ANON_KEY_TOKEN,
-      adminPassword:  process.env.ADMIN_PASSWORD_TOKEN,
-      rss2jsonKey:    process.env.RSS2JSON_KEY_TOKEN
+      supabaseUrl:    process.env.SUPABASE_URL,
+      supabaseKey:    process.env.SUPABASE_ANON_KEY,
+      adminPassword:  process.env.ADMIN_PASSWORD,
+      rss2jsonKey:    process.env.RSS2JSON_KEY
     })
   };
 };
