9ae3cb49bf717d331497c251d7f24abd000a665e
samdriver2491 <74094719+samdriver2491@users.noreply.github.com>
2026-03-09 16:15:58 +0000

    Update ReliefWeb API endpoint and query parameters

--- netlify/functions/reliefweb.js
+++ netlify/functions/reliefweb.js
@@ -1,9 +1,9 @@
 exports.handler = async () => {
-  const response = await fetch('https://api.reliefweb.int/v1/reports?appname=apidoc-demo', {
+  const response = await fetch('https://api.reliefweb.int/v2/reports?appname=rwint-user-0&profile=list&preset=latest&slim=1&query%5Bvalue%5D=disease+epidemic+outbreak&query%5Boperator%5D=OR', {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify({
-      query: { value: "disease outbreak", operator: "AND" },
+      query: { value: "disease epidemic outbreak", operator: "OR" },
       fields: { include: ["title", "url", "date", "body-html"] },
       sort: ["date:desc"],
       limit: 15
